4c65a6d70246c4c1832e745449703ee3de3599cc
Daniel <38142096+adaniel2@users.noreply.github.com>
2019-04-07 00:21:40 -0400

    Parser Implementation
    Implementation of parser, ID is working, key might be off (not sure how to do 128 bit integers)

--- src/BinaryParser.java
+++ src/BinaryParser.java
@@ -5,24 +5,39 @@ import java.io.RandomAccessFile;
 public class BinaryParser {
 
     public BinaryParser(String fileName) throws IOException {
-
-        long key;
-        double value;
-        boolean i = true;
+        int byteCount = 0;
+        
         RandomAccessFile raf = new RandomAccessFile(fileName, "r");
-        while (i) {
+        
+        while (byteCount < 128) {
+            long idSum = 0;
+            double keySum = 0;
+            
             try {
-                key = raf.readLong();
-                value = raf.readDouble();
-                System.out.println(key);
-                System.out.println(value);
-
+                // grab first 8 bytes
+                for (int i = 7; i >= 0; i--) {
+                    long temp = ((long)raf.readUnsignedByte() << (i * 8));
+                    idSum += temp;
+                    byteCount++;
+                }
+                
+                // grab second 8 bytes
+                for (int i = 7; i >= 0; i--) {
+                    double temp = ((long)raf.readByte() << (i * 8));
+                    keySum += temp;
+                    byteCount++;
+                }
+                
+                // print record (16 bytes)
+                System.out.println(idSum + " and " + keySum);
+                
             }
             catch (EOFException e) {
-                i = false;
+                
             }
 
         }
+        
         raf.close();
     }
 }
