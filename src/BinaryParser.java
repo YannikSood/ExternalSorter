0eda7d6457e604b5a4005e02a3689c1d7cea3f59
Daniel <38142096+adaniel2@users.noreply.github.com>
2019-04-10 20:18:16 -0400

    Parser Work
    Made edits to parser, it is grabbing the bytes correctly, but not sure if we're expected to print the correct decimal representations of these.

--- src/BinaryParser.java
+++ src/BinaryParser.java
@@ -8,32 +8,40 @@ public class BinaryParser {
         int byteCount = 0;
         
         RandomAccessFile raf = new RandomAccessFile(fileName, "r");
+        Boolean endFile = false;
         
-        while (byteCount < 128) {
+        while (!endFile) {
             long idSum = 0;
             double keySum = 0;
+            int completeRecord = 0;
             
             try {
                 // grab first 8 bytes
                 for (int i = 7; i >= 0; i--) {
                     long temp = ((long)raf.readUnsignedByte() << (i * 8));
                     idSum += temp;
+                    
                     byteCount++;
+                    completeRecord += 8;
                 }
+                
+                // print first 8 bytes
+                System.out.print(idSum + " and ");
                 
                 // grab second 8 bytes
                 for (int i = 7; i >= 0; i--) {
                     double temp = ((long)raf.readByte() << (i * 8));
                     keySum += temp;
+                    
                     byteCount++;
+                    completeRecord += 8;
                 }
                 
-                // print record (16 bytes)
-                System.out.println(idSum + " and " + keySum);
-                
+                // print second 8 bytes and new line
+                System.out.println(keySum);
             }
             catch (EOFException e) {
-                
+                endFile = true;
             }
 
         }
