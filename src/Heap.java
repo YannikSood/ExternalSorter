fc6459ebba6c0bd57cdfd6eb4597c21f465d7222
Daniel <38142096+adaniel2@users.noreply.github.com>
2019-04-14 12:05:09 -0400

    Design/Style and Testing Points.

--- src/Heap.java
+++ src/Heap.java
@@ -10,15 +10,15 @@ public class Heap {
     private Record[] minHeap;
     private int maxSize; // max size of heap
     private int nHeap; // no. elements in heap
-    
+
     /**
      * Creates an empty Heap.
      */
-   public Heap() {
-       this.maxSize = 512 * 8;
-       this.minHeap = new Record[maxSize];
-       this.nHeap = 0;
-   }
+    public Heap() {
+        this.maxSize = 512 * 8;
+        this.minHeap = new Record[maxSize];
+        this.nHeap = 0;
+    }
 
     /**
      * Creates a pre-loaded Heap.
@@ -35,29 +35,29 @@ public class Heap {
         byte[] key;
         byte[] value;
         int byteCount = 0;
-        
+
         while (byteCount < n) {
             // if you don't do this, it will reference to the same array
             // in each record
             key = new byte[8];
             value = new byte[8];
-            
+
             // grab key
             for (int i = 0; i < 8; i++) {
                 key[i] = byteArray[byteCount];
                 byteCount++;
             }
-            
+
             // grab value
             for (int i = 0; i < 8; i++) {
                 value[i] = byteArray[byteCount];
                 byteCount++;
             }
-            
+
             // assign record
             minHeap[nHeap++] = new Record(key, value);
         }
-        
+
         // heapify
         this.makeMinHeap();
     }
@@ -70,7 +70,7 @@ public class Heap {
     public int getSize() {
         return this.nHeap;
     }
-    
+
     /**
      * Special case setSize() for replacement sorting of dead nodes.
      * WARNING: USE WITH CAUTION.
@@ -105,9 +105,9 @@ public class Heap {
             return -1;
         }
 
-      return 2*pos + 1;
+        return 2 * pos + 1;
     }
-    
+
     /**
      * Return position for right child of pos
      * 
@@ -122,7 +122,7 @@ public class Heap {
             return -1;
         }
 
-        return 2*pos + 2;
+        return 2 * pos + 2;
     }
 
     /**
@@ -149,16 +149,16 @@ public class Heap {
             return;
         }
 
-        int curr = nHeap++; // this assigns current nHeap then increments
-        minHeap[curr] = record; // start at end of heap
+        int cur = nHeap++; // this assigns current nHeap then increments
+        minHeap[cur] = record; // start at end of heap
 
         // sifting until curr's parent's key < curr's key
-        while ((curr != 0) && minHeap[curr].compareTo(minHeap[parent(curr)]) < 0) {
-            swapNodes(curr, parent(curr));
-            curr = parent(curr);
+        while ((cur != 0) && minHeap[cur].compareTo(minHeap[parent(cur)]) < 0) {
+            swapNodes(cur, parent(cur));
+            cur = parent(cur);
         }
     }
-    
+
     /**
      * Remove and return record at pos.
      * 
@@ -169,7 +169,7 @@ public class Heap {
         if ((pos < 0) || (pos >= nHeap)) {
             return null;
         }
-        
+
         if (pos == (nHeap - 1)) {
             nHeap--; // just decrease the size
             return minHeap[nHeap]; // return removed element
@@ -180,16 +180,16 @@ public class Heap {
             return minHeap[nHeap]; // return removed element
         }
     }
-    
+
     /**
      * Return the root value of the heap.
      * 
      * @return      record at root of heap.
      */
     public Record getRoot() {
-            return minHeap[0]; // return root element
+        return minHeap[0]; // return root element
     }
-    
+
     /**
      * Value of pos has been changed, restore heap property.
      * 
@@ -201,13 +201,13 @@ public class Heap {
             swapNodes(pos, parent(pos));
             pos = parent(pos);
         }
-        
+
         // if it's larger then sift down if needed
         if (nHeap != 0) {
             siftDown(pos);
         }
     }
-    
+
     /**
      * Modify value at given position.
      * 
@@ -218,11 +218,11 @@ public class Heap {
         if ((pos < 0) || (pos >= nHeap)) {
             return;
         }
-        
+
         minHeap[pos] = newVale;
         update(pos);
     }
-    
+
     /**
      * Special case modify for replacement sorting, does not call update.
      * 
@@ -232,7 +232,7 @@ public class Heap {
     public void rSortMod(int pos, Record newVale) {
         minHeap[pos] = newVale;
     }
-    
+
     /**
      * Remove min value from heap.
      *
@@ -242,13 +242,13 @@ public class Heap {
         if (nHeap == 0) {
             return null;
         }
-        
+
         swapNodes(0, --nHeap); // also decrements heap size
-        
+
         if (nHeap != 0) { // not last element
             siftDown(0);
         }
-        
+
         return minHeap[nHeap];
     }
 
@@ -270,20 +270,21 @@ public class Heap {
         if ((pos < 0) || (pos >= nHeap)) {
             return;
         }
-        
+
         while (!isLeaf(pos)) {
             int j = leftChild(pos);
-            
+
             // checking which node to pick
-            if ((j < (nHeap-1)) && (minHeap[j].compareTo(minHeap[j+1]) > 0)) {
-                j++; // index of child with lower value
+            if ((j < (nHeap - 1))
+                && (minHeap[j].compareTo(minHeap[j +1 ]) > 0)) {
+                    j++; // index of child with lower value
             }
-            
+
             // does this if second child does not exist or is greater/equal
             if (minHeap[j].compareTo(minHeap[pos]) >= 0) {
                 return; // the child j is greater than or equal to pos
             }
-            
+
             // swap depending on above outcomes
             swapNodes(pos, j);
             pos = j;
